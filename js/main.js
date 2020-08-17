943e0d711f012fde4b456f5553d9571e8349323d
Lucy Wasiek <lucy@MacBook-Pro-de-Lucy.local>
2020-08-17 13:19:33 +0200

    add links to menu buttons and change the logo

--- js/main.js
+++ js/main.js
@@ -27,3 +27,13 @@ jQuery(document).ready(function(){
     });
 
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").style.fontSize = "30px";
+  } else {
+    document.getElementById("header").style.fontSize = "90px";
+  }
+}
